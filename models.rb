8646afc7db07cbe5d5d0774c1fc2987f9e1caf46
Tsukasa <tsukasa.php@gmail.com>
2018-09-16 02:44:02 +0900

    first commit

---
+++ models.rb
@@ -0,0 +1,13 @@
+# require 'bundler/setup'
+# Bundler.require
+
+ActiveRecord::Base.establish_connection('sqlite3:db/development.db')
+
+class User < ActiveRecord::Base
+  has_secure_password
+  validates :mail,
+    presence: true,
+    format: {with:/.+@.+/}
+  validates :password,
+    length: {in: 5..10}
+end
